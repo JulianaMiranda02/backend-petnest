5aefc074011d57df64e65cf332d8f0b1a2761327
juliana miranda <mirianjuliana35@gmail.com>
2026-01-13 21:28:13 -0300

    criacao de endpoints

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 const http = require("http")
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, { "Content-Type": "text/plain" })
-    response.end("Dados de Animais")
+const server = http.createServer((pedido, resposta) => {
+    // dentro da variavel pedido (objeto) tem uma propriedade (chave) method e url
+    // method vai ter métod do pedido (se é GET, POST ou DELETE)
+    // url vai ter o endpoint: ex: '/animais-perdidos'
+
+    // se for um pedido no endpoint '/animais-perdidos' 
+    if (pedido.url === "/animais-perdidos" && pedido.method === "GET") {
+        console.log("Pedido de Animais Perdidos");
+        resposta.writeHead(200, { "Content-Type": "text/plain" })
+        resposta.end("Pedido de Animais Perdidos")
+    }
+
+    // se for um pedido no endpoint '/sinalizar-animal' 
+    else if (pedido.url === "/sinalizar-animal" && pedido.method === "POST") {
+        console.log("Pedido de Sinalizar Animal");
+        resposta.writeHead(200, { "Content-Type": "text/plain" })
+        resposta.end("Pedido de Sinalizar Animal")
+    }
+
+    // se for um pedido no endpoint '/deletar-animal'
+    else if (pedido.url === "/deletar-animal" && pedido.method === "DELETE") {
+        console.log("Pedido de Deletar Animal");
+        resposta.writeHead(200, { "Content-Type": "text/plain" })
+        resposta.end("Pedido de Deletar Animal")
+    }
 })
 
 server.listen(1234, () => {
