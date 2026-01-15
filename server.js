e455fc65177473eebaaaf7ac686558d26cd1b38d
juliana miranda <mirianjuliana35@gmail.com>
2026-01-14 22:01:32 -0300

    insercao de funcoes

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const http = require("http")
 
+function animaisPerdidos(resposta) {
+    console.log("Pedido de Animais Perdidos");
+    resposta.writeHead(200, { "Content-Type": "text/plain" })
+    resposta.end("Pedido de Animais Perdidos")
+}
+
+function sinalizarAnimal(resposta) {
+    console.log("Pedido de Sinalizar Animal");
+    resposta.writeHead(200, { "Content-Type": "text/plain" })
+    resposta.end("Pedido de Sinalizar Animal")
+}
+
+function deletarAnimal(resposta) {
+    console.log("Pedido de Deletar Animal");
+    resposta.writeHead(200, { "Content-Type": "text/plain" })
+    resposta.end("Pedido de Deletar Animal")
+}
+
 const server = http.createServer((pedido, resposta) => {
     // dentro da variavel pedido (objeto) tem uma propriedade (chave) method e url
     // method vai ter métod do pedido (se é GET, POST ou DELETE)
@@ -7,23 +25,17 @@ const server = http.createServer((pedido, resposta) => {
 
     // se for um pedido no endpoint '/animais-perdidos' 
     if (pedido.url === "/animais-perdidos" && pedido.method === "GET") {
-        console.log("Pedido de Animais Perdidos");
-        resposta.writeHead(200, { "Content-Type": "text/plain" })
-        resposta.end("Pedido de Animais Perdidos")
+        animaisPerdidos(resposta)
     }
 
     // se for um pedido no endpoint '/sinalizar-animal' 
     else if (pedido.url === "/sinalizar-animal" && pedido.method === "POST") {
-        console.log("Pedido de Sinalizar Animal");
-        resposta.writeHead(200, { "Content-Type": "text/plain" })
-        resposta.end("Pedido de Sinalizar Animal")
+        sinalizarAnimal(resposta)
     }
 
     // se for um pedido no endpoint '/deletar-animal'
     else if (pedido.url === "/deletar-animal" && pedido.method === "DELETE") {
-        console.log("Pedido de Deletar Animal");
-        resposta.writeHead(200, { "Content-Type": "text/plain" })
-        resposta.end("Pedido de Deletar Animal")
+        deletarAnimal(resposta)
     }
 })
 
