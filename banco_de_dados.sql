6f4d553c129e18e5f786fdebf9f7a4f308c3fb9d
juliana miranda <mirianjuliana35@gmail.com>
2026-01-27 22:46:28 -0300

    atualizacao esquema banco de dados

--- banco_de_dados.sql
+++ banco_de_dados.sql
@@ -3,19 +3,19 @@ create database petnest;
 use petnest;
 
 create table animais_perdidos(
-id int not null auto_increment primary key,
-nome_sinalizador varchar(255),
-telefone varchar(20),
-sexo char(1) check (sexo in ('M','F')),
-tipo varchar(15),
-porte varchar (15),
-descricao text,
-link_foto varchar(255),
-rua varchar (255),
-numero varchar(10),
-bairro varchar(40),
-cidade varchar(20),
-estado char(2),
-cep varchar(9)
+id int not null auto_increment primary key not null,
+nome_sinalizador varchar(255) not null,
+telefone varchar(20) not null,
+sexo enum('M','F') not null,
+tipo varchar(15) not null,
+porte varchar (15) not null,
+descricao text not null,
+link_foto varchar(255) not null,
+rua varchar (255) not null,
+numero varchar(10) not null,
+bairro varchar(40) not null,
+cidade varchar(20) not null,
+estado char(2) not null,
+cep varchar(9) not null
 );
 
